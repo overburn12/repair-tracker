75ce581aa322f3cfbffc1a3fa4194706eba56780
overburn <overburn12@gmail.com>
2025-10-14 23:43:20 -0400

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(host='::', port=6969)
