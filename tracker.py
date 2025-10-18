12bb367902e9398c1df95342d8d933362c5cdd93
overburn <overburn12@gmail.com>
2025-10-18 15:49:57 -0400

    init some classes for data

--- tracker.py
+++ tracker.py
@@ -1,10 +1,49 @@
-from dataclasses import dataclass
 
-@dataclass
-class repair_order:
+class RepairOrder:
     def __init__(self):
-        #
+        self.key = None
+        self.name = None
+        self.status = None
+        self.created = None
+        self.recieved = None
+        self.machines = []
+        self.hashboards = []
+
+    def load(self, json):
+        pass
     
 
-@dataclass
-class 
+class Machine:
+    def __init__(self):
+        self.key = None
+        self.serial = None
+        self.events = []
+
+    def load(self, json):
+        pass
+
+
+class Hashboard:
+    def __init__(self):
+        self.key = None
+        self.serial = None
+        self.events = []
+
+    def load(self, json):
+        pass
+
+
+class Event:
+    def __init__(self):
+        self.type = None
+        self.assignee = None
+        self.timestamp = None
+
+        self.comment = None
+        self.status = None
+        self.components = None
+
+
+    def load(self, json):
+        pass
+
